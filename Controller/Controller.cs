57bb70ef29a1ea82c2eafe10d20a01f1e0fd0dfb
danielpatricio0022 <hadukens@hotmail.com>
2024-11-12 13:26:41 -0300

    .

---
+++ Controller/Controller.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ApiNet.model;
+
+namespace ApiNet.Controller
+{
+    [Route("/api[controller]")]
+    [ApiController]
+    public class Controller(HttpClient httpClient) : ControllerBase
+    {
+        // GET: /tasks
+        [HttpGet("/tasks")]
+        public async Task<IActionResult> GetTasks()
+        {
+            var url = "http://localhost:3001/api/tasks";
+            var result = await httpClient.GetAsync(url);
+
+            if (result.StatusCode == System.Net.HttpStatusCode.OK &&
+                result.Content.Headers.ContentType?.MediaType == "application/json")
+            {
+               
+                var content = await result.Content.ReadFromJsonAsync<List<TaskDTO>>();
+                return Ok(content); // Return 200
+            }
+            else
+            {
+                return NotFound(); // Return 404
+            }
+        }
+
+        // GET: /tasks/{id}
+        [HttpGet("/tasks/{id}")]
+        public async Task<IActionResult> GetTaskById(int id)
+        {
+            var url = $"http://localhost:3001/api/tasks/{id}";
+            var result = await httpClient.GetAsync(url);
+
+            if (result.StatusCode == System.Net.HttpStatusCode.OK &&
+                result.Content.Headers.ContentType?.MediaType == "application/json")
+            {
+                var task = await result.Content.ReadFromJsonAsync<TaskDTO>(); 
+                if (task != null && task.id == id)
+                {
+                    return Ok(task); // Return 200
+                }
+            }
+
+            return NotFound(); // Return 404
+        }
+
+        // PUT: /tasks/{id}
+        [HttpPut("/tasks/{id}")]
+        public async Task<IActionResult> UpdateTask(int id, [FromBody] TaskDTO taskRequestDto)
+        {
+            var url = $"http://localhost:3001/api/tasks/{id}";
+            var result = await httpClient.PutAsJsonAsync(url, taskRequestDto);
+
+            if (result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var task = await result.Content.ReadFromJsonAsync<TaskDTO>(); 
+                if (task != null && task.id == id)
+                {
+                    return Ok(task); // Return 200 
+                }
+            }
+
+            if (result.StatusCode == System.Net.HttpStatusCode.NoContent)
+            {
+                return NoContent(); // Return 204
+            }
+
+            return Problem("Failed to update task", statusCode: 500); //500
+        }
+
+        // POST: /tasks
+        [HttpPost("/tasks")]
+        public async Task<IActionResult> CreateTask([FromBody] TaskDTO taskRequestDto)
+        {
+            var url = "http://localhost:3001/api/tasks";
+            var result = await httpClient.PostAsJsonAsync(url, taskRequestDto);
+
+            if (result.IsSuccessStatusCode &&
+                result.Content.Headers.ContentType?.MediaType == "application/json")
+            {
+                var createdTask = await result.Content.ReadFromJsonAsync<TaskDTO>(); 
+                if (createdTask != null)
+                {
+                    return CreatedAtAction(
+                        nameof(GetTaskById), new { id = createdTask.id },
+                        createdTask);
+                }
+
+                return Problem("Failed to deserialize response", statusCode: 500);
+            }
+
+            return Problem("Failed to create task", statusCode: 500); // Return 500
+        }
+
+        // DELETE: /tasks/{id}
+        [HttpDelete("/tasks/{id}")]
+        public async Task<IActionResult> DeleteTask(int id)
+        {
+            var url = $"http://localhost:3001/api/tasks/{id}";
+            var result = await httpClient.DeleteAsync(url);
+
+            if (result.StatusCode == System.Net.HttpStatusCode.NoContent)
+            {
+                return NoContent(); // Return 204 
+            }
+            else if (result.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return NotFound(); // Return 404 
+            }
+            else
+            {
+                return Problem("Failed to delete task", statusCode: 500); // Return 500
+            }
+        }
+    }
+}
---
+++ model/TaskDTO.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiNet.model;
+
+public class TaskDTO // the validations must be done in backend
+{
+   
+    public int id { get; set; }
+    
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(50, ErrorMessage = "Name is too long")] // in time of execution
+    public string name { get; set; }
+    
+    [Required(ErrorMessage = "Description is required")]
+    [StringLength(200, ErrorMessage = "Description is too long")]
+    public string description { get; set; }
+    
+    
+    [Required(ErrorMessage = "Done is required")]
+    public bool done { get; set; } 
+    
+    [DataType(DataType.Date, ErrorMessage = "Invalid date")]
+    public DateTime date { get; set; } // ;P
+    // ;P
+    
+    public TaskDTO() { } // serializations and desserializations
+    
+
+    public override string ToString()
+    {
+        return
+            $"{nameof(id)}: {id}, {nameof(name)}: {name}, {nameof(date)}: {date}, {nameof(description)}: {description}, {nameof(done)}: {done}";
+    }
+}
