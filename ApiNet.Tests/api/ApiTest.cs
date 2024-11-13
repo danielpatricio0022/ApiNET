d8e2a8f6081bc9d6ad685ac1f0430ff5e83fef6e
danielpatricio0022 <hadukens@hotmail.com>
2024-11-13 09:01:09 -0300

    modified

---
+++ ApiNet.Tests/api/ApiTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using ApiNet.model;
+using Xunit;
+
+namespace ApiNet.Tests.api;
+
+public class ApiTest
+{
+    private readonly HttpClient httpClient;
+    
+    public ApiTest( )
+    {
+        httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("http://localhost:5119")
+            
+        }; 
+    }
+    
+    [Fact]
+    public async void GetTasks()
+    {
+        var response = await httpClient.GetAsync("tasks");
+        var data = await response.Content.ReadFromJsonAsync<List<TaskDTO>>();
+        
+       
+        
+        if (response.Content.Headers.ContentType != null)
+            Assert.Equal("application/json", response.Content.Headers.ContentType.MediaType);
+        
+        Assert.True(response.IsSuccessStatusCode);
+        Assert.NotNull(data);
+    }
+    
+    [Fact]
+    public async void GetTaskById()
+    {
+        var response = await httpClient.GetAsync("tasks/3");
+        var data = await response.Content.ReadFromJsonAsync<TaskDTO>();
+        
+        if (response.Content.Headers.ContentType != null)
+            Assert.Equal("application/json", response.Content.Headers.ContentType.MediaType);
+        
+        Assert.True(response.IsSuccessStatusCode);
+        Assert.NotNull(data);
+    }
+    
+    
+    [Fact]
+    public async void UpdateTask()
+    {
+        var task = new TaskDTO()
+        {
+            name = "Task 3",
+            description = "Description 3",
+            done = false,
+            date = DateTime.Now
+        };
+        
+        var response = await httpClient.PutAsJsonAsync("tasks/3", task);
+        var data = await response.Content.ReadFromJsonAsync<TaskDTO>();
+        
+        if (response.Content.Headers.ContentType != null)
+            Assert.Equal("application/json", response.Content.Headers.ContentType.MediaType);
+        
+        Assert.True(response.IsSuccessStatusCode);
+        Assert.NotNull(data);
+    }
+    
+}
---
+++ ApiNet.Tests/model/TaskDTOTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using ApiNet.model;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace ApiNet.Tests.model;
+
+[TestSubject(typeof(TaskDTO))]
+public class TaskDTOTest
+{
+    [Fact]
+    public void TaskDTO_Validation_WhenNameIsEmpty()
+    {
+        // Arrangee
+        var task = new TaskDTO
+        {
+            name = "",
+            description = "Description",
+            done = false,
+            date = DateTime.Now
+        };
+
+        // when
+        var validationResults = new List<ValidationResult>(); // list of validation results
+        var validationContext = new ValidationContext(task, null, null);
+                                         //object to validate, injection null and dictionary of items null
+                                         
+        Validator.TryValidateObject(task, validationContext, validationResults, true);
+
+        // Assert
+        Assert.Contains(validationResults, v => v.ErrorMessage == "Name is required" && 
+                                                              v.MemberNames.Contains("name"));
+        
+
+    }
+    
+    
+    
+    
+
+}
